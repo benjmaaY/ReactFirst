508b8dff77318b11231c87de34ee435d2079287f
benjmaaY <40885051+benjmaaY@users.noreply.github.com>
2018-08-06 19:03:24 +0200

    Blablablablablablablabla

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const image1 = "https://i.imgur.com/FS24iEO.png"
+    return (
+      <div>
+        <img src={image1} alt="Test" />
+        <p className="Class1" >Loreeeeeem hsbgdidhjb fyhds vgfuysd dfgut sd futsdd futsdf utisdftsydf std fds f</p>
+        <br /> <hr />
+        < p className = "Class2" >jhgfyu gsdyufgydg syidfg oisygf yifdsf gyi gfiyf gfiydg </p>
+        <br /> <hr />
+      </div>
+    );
+  }
+}
+
+export default App;
